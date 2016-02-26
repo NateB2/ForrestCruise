55ab5b563bb128e5ffb96e62022ef767a31e9353
Daniel Sauble <djsauble@gmail.com>
2016-02-25 20:29:22 -0800

    Add model for routes

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@
 
 require 'rubygems'
 require 'sinatra'
-require 'haml'
+require 'tilt/haml'
 require 'dm-core'
 require 'dm-migrations'
 require 'digest/sha1'
 require 'sinatra-authentication'
+require_relative 'models/route'
 
 # Connect to our MySQL database
 DataMapper.setup(:default, "#{ENV["CLEARDB_DATABASE_URL"]}/#{ENV["CLEARDB_DATABASE_NAME"]}?reconnect=true")
