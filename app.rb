51a53625b8d950bce7d63da6e1e13568b0f5f9c3
Daniel Sauble <djsauble@gmail.com>
2016-02-25 20:07:21 -0800

    Add some clarifying comments

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ require 'dm-core'
 require 'dm-migrations'
 require 'digest/sha1'
 require 'sinatra-authentication'
+
+# Connect to our MySQL database
 DataMapper.setup(:default, "#{ENV["CLEARDB_DATABASE_URL"]}/#{ENV["CLEARDB_DATABASE_NAME"]}?reconnect=true")
 DataMapper.auto_upgrade!
+
+# Configure the session cookie
 use Rack::Session::Cookie, :secret => "#{ENV["CACHE_SECRET"]}"
 
 get '/' do
