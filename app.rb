dc16022b50ec14010bfae9eaac6010208e672ee1
Daniel Sauble <djsauble@gmail.com>
2016-02-25 17:52:49 -0800

    Allow the database name to be customized

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'dm-core'
 require 'dm-migrations'
 require 'digest/sha1'
 require 'sinatra-authentication'
-DataMapper.setup(:default, "#{ENV["CLEARDB_DATABASE_URL"]}/forrest")
+DataMapper.setup(:default, "#{ENV["CLEARDB_DATABASE_URL"]}/#{ENV["CLEARDB_DATABASE_NAME"]}?reconnect=true")
 DataMapper.auto_upgrade!
 use Rack::Session::Cookie, :secret => "#{ENV["CACHE_SECRET"]}"
 
