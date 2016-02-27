e574b49d3e295a625d56c082c3056cdee1fdc999
Daniel Sauble <djsauble@gmail.com>
2016-02-26 18:01:12 -0800

    Make the index page more helpful
    + Show the number of routes a user has added to the database
    + Provide a sample curl command
    + Give instructions on what to do if a token is compromised

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 require 'tilt/haml'
 require 'dm-core'
 require 'dm-migrations'
+require 'dm-aggregates'
 require 'digest/sha1'
 require 'sinatra-authentication'
 
@@ -20,15 +21,26 @@ use Rack::Session::Cookie, :secret => "#{ENV["CACHE_SECRET"]}"
 
 get '/' do
   if logged_in?
-    @url = "/users/#{current_user.id}/#{current_user.hashed_password}#{current_user.salt}"
-    haml :index, :locals => {:url => @url}
+    # Construct a unique token URL based on password hash and salt
+    @base = request.url
+    @url = "users/#{current_user.id}/#{current_user.hashed_password}#{current_user.salt}"
+
+    # Count the number of routes this user has added to the database
+    @count = Route.count(:user => current_user.id)
+
+    # Render the view
+    haml :index, :locals => {:base => @base, :url => @url, :count => @count}
   else
     redirect '/login'
   end
 end
 
+# Upload a new route to the database
 post '/users/:id/:token' do
+  # Get the record for the specified user
   @user = DmUser.get(params['id'])
+
+  # Does the token match the specified user's password hash and salt?
   if params['token'] == "#{@user.hashed_password}#{@user.salt}"
     Route.create(
       :user => params['id'],
